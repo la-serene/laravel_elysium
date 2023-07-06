e3f084742127eacd0ae0851f4743c64ca67525cf
Nguyen Cao Ky <ky.nguyencao23@gmail.com>
2023-07-06 19:55:19 +0700

    add breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,17 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::name('welcome')
-    ->get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::controller(LoginController::class)
-    ->group(function () {
-       Route::get('/login', 'login')->name('login');
-       Route::get('/register', 'register')->name('register');
-        Route::post('/authenticate', 'authenticate')->name('authenticate');
-        Route::post('/store', 'store')->name('store');
-    });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
