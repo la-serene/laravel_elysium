e35edf43d3ddb84d1ea644c21b8e9f338207daf4
tra-nguyen <tranguyen.contact@gmail.com>
2023-07-12 10:12:02 +0700

    Add comment rating

--- database/migrations/2023_07_09_132929_create_order_details_table.php
+++ database/migrations/2023_07_09_132929_create_order_details_table.php
@@ -9,16 +9,21 @@ class CreateOrderDetailsTable extends Migration
     public function up()
     {
         Schema::create('order_details', function (Blueprint $table) {
-            $table->id();
             $table->unsignedBigInteger('order_id');
-            $table->foreign('order_id')->references('id')->on('orders');
             $table->unsignedBigInteger('product_option_id');
-            $table->foreign('product_option_id')->references('id')->on('product_options');
             $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('products');
             $table->integer('quantity');
             $table->decimal('total_price', 8, 2);
+
+            //review field
+            $table->text('comment');
+            $table->integer('rating');
             $table->timestamps();
+
+            $table->primary(['order_id', 'product_option_id', 'product_id']);
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_option_id')->references('id')->on('product_options');
+            $table->foreign('product_id')->references('id')->on('products');
         });
     }
 
