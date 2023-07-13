5d84c5e02f45a1ca043d2b5f2770aa7fcfa0b0a1
Nguyen Cao Ky <ky.nguyencao23@gmail.com>
2023-07-13 17:08:36 +0700

    ui

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -11,11 +11,6 @@
             <ul class="main-menu">
                 <li class="active-menu">
                     <a href="{{ route('welcome') }}">Home</a>
-                    <ul class="sub-menu">
-                        <li><a href="index.html">Homepage 1</a></li>
-                        <li><a href="home-02.html">Homepage 2</a></li>
-                        <li><a href="home-03.html">Homepage 3</a></li>
-                    </ul>
                 </li>
 
                 <li>
