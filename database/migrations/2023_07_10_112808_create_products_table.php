6137cec72e7e8ab9eff943694c2d125883759956
Nguyen Cao Ky <ky.nguyencao23@gmail.com>
2023-07-18 04:30:19 +0700

    [Update] update product table schema

--- database/migrations/2023_07_10_112808_create_products_table.php
+++ database/migrations/2023_07_10_112808_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product_name');
+            $table->string('product_description');
+            $table->decimal('product_price', 8, 2);
+            $table->decimal('rating', 5, 2);
+            $table->integer('total_sales')->default(0);
+            $table->integer('in_stock');
             $table->timestamps();
         });
     }
