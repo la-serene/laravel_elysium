b7538ec06649a7ab3753c0582588eee8e0b24ccb
Nguyen Cao Ky <ky.nguyencao23@gmail.com>
2023-07-18 02:13:25 +0700

    [Update] change user table design

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('address')->nullable();
             $table->date('date_of_birth');
             $table->string('email')->unique();
+            $table->string('avatar')->default("https://www.pinterest.com/pin/polite-cat-meme-is-this-viral-image-of--670895675717634661/");
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
