6b85b6a78adb19dd1fb8b0eba7c27eaf09e85cce
Nguyen Cao Ky <ky.nguyencao23@gmail.com>
2023-07-17 10:44:32 +0700

    [Update] add auth register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,28 @@
 @extends('auth.layout')
 @section('content')
-    <form action="">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('authenticate') }}" method="POST">
+        @csrf
         <h3>LOGIN</h3>
         <div class="form-wrapper">
-            <input type="text" placeholder="Email Address" class="form-control">
+            <label>
+                <input type="text" placeholder="Email Address" class="form-control" name="email">
+            </label>
             <i class="zmdi zmdi-email"></i>
         </div>
 
         <div class="form-wrapper">
-            <input type="password" placeholder="Password" class="form-control">
+            <label>
+                <input type="password" placeholder="Password" class="form-control" name="password">
+            </label>
             <i class="zmdi zmdi-lock"></i>
         </div>
         <a href="forget.html" class="forget-password">
@@ -19,7 +33,7 @@
         <button>Login
             <i class="zmdi zmdi-arrow-right"></i>
         </button>
-        <a href="register.html" style="color: gray; padding-top: 20px ; padding-left: 5px;">
+        <a href="{{ route('register') }}" style="color: gray; padding-top: 20px ; padding-left: 5px;">
             You don't have an account? Register now!
         </a>
     </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,57 @@
-<?php
+@extends('auth.layout')
+@section('content')
+    <form action="{{ route('store') }}" method="POST">
+        @csrf
+        <h3>Registration Form</h3>
+        <div class="form-group">
+            <label>
+                <input type="text" placeholder="First Name" class="form-control">
+            </label>
+            <label>
+                <input type="text" placeholder="Last Name" class="form-control">
+            </label>
+        </div>
+        <div class="form-wrapper">
+            <label>
+                <input type="text" placeholder="Username" class="form-control">
+            </label>
+            <i class="zmdi zmdi-account"></i>
+        </div>
+        <div class="form-wrapper">
+            <label>
+                <input type="tel" placeholder="Phone number" class="form-control">
+            </label>
+        </div>
+        <div class="form-wrapper">
+            <label>
+                <input type="text" placeholder="Address" class="form-control">
+            </label>
+        </div>
+        <div class="form-wrapper">
+            <label>
+                <input type="text" placeholder="Email Address" class="form-control">
+            </label>
+            <i class="zmdi zmdi-email"></i>
+        </div>
+        <div class="form-wrapper">
+            <label>
+                <input type="date" placeholder="Date Of Birth" class="form-control">
+            </label>
+        </div>
+        <div class="form-wrapper">
+            <label>
+                <input type="password" placeholder="Password" class="form-control">
+            </label>
+            <i class="zmdi zmdi-lock"></i>
+        </div>
+        <div class="form-wrapper">
+            <label>
+                <input type="password" placeholder="Confirm Password" class="form-control">
+            </label>
+            <i class="zmdi zmdi-lock"></i>
+        </div>
+        <button>Register
+            <i class="zmdi zmdi-arrow-right"></i>
+        </button>
+    </form>
+@endsection
